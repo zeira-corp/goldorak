083fc921457aa7f5dfdf50f894464000946f0e0d
Daniel Petisme <daniel.petisme@gmail.com>
2017-01-24 11:26:30 +0100

    Fix deploy

--- gulp_tasks/deploy.js
+++ gulp_tasks/deploy.js
@@ -1,9 +1,11 @@
-var gulp        = require('gulp');
-var deployGH    = require('gulp-gh-pages');
+var gulp = require('gulp');
+var deployGH = require('gulp-gh-pages');
+
+const conf = require('../conf/gulp.conf');
 
 gulp.task('deploy', deploy);
 
 function deploy() {
-  return gulp.src("./dist/**/*")
+  return gulp.src(conf.path.dist("/**/*"))
     .pipe(deployGH());
 }
